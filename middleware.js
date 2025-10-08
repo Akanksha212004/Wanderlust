9aa888660c6e401143061691006382ded54a4d0c
Akanksha Yadav <akankshayadav212004@gmail.com>
2025-10-09 02:07:07 +0530

    Delete button visiblity limited for reviews

--- middleware.js
+++ middleware.js
@@ -72,12 +72,18 @@ module.exports.validateReview = (req, res, next) => {
 module.exports.isReviewAuthor = async (req, res, next) => {
     let { id, reviewId } = req.params;
     let review = await Review.findById(reviewId);
-    if(!review.author.equals(res.locals.currUser._id)) {
+
+    // Check if review exists
+    if (!review) {
+        req.flash("error", "Review not found");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    // Compare the review's author with the currently logged-in user
+    if(!review.author.equals(req.user._id)) {
         req.flash("error", "You are not the author of this review");
         return res.redirect(`/listings/${id}`);
     }
 
     next();
 };
-
-
