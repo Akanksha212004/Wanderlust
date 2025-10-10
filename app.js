510ddd682ed9e00f8e09cb45e0663baa392d44c3
Akanksha Yadav <akankshayadav212004@gmail.com>
2025-10-11 01:29:03 +0530

    Added styling to make it responsiveness

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
-    res.locals.search = "";    // ✅ initialize 'search' for navbar.ejs
+    res.locals.search = "";    // initialize 'search' for navbar.ejs
     next();
 });
 
